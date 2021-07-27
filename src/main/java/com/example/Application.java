1a39100f009e4540a2a67707f2be4f4a543cc5bf
심진영 <wrk94@naver.com>
2021-07-28 07:17:56 +0900

    commit~~~

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static ApplicationContext applicationContext;
+    public static void main(String[] args) {
+        applicationContext = SpringApplication.run(Application.class, args);
+        System.out.println("dddd");
+    }
+
+
+}
+
+
